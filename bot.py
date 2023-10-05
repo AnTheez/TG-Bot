2a18ee2189f8279c6c06ac109c2afcba6733c11c
Aiter <vsimvs.17879@gmail.com>
2023-10-05 13:39:34 +0300

    квиз commit

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
 import random
 import mysql.connector
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, PollAnswerHandler
 
 # Import your database and token reader functions
 from database.database_config import get_database_connection
@@ -14,8 +14,8 @@ TOKEN = get_bot_token()
 # Establish a database connection using the function from database_config.py
 db_connection = get_database_connection()
 
-# Initialize a dictionary to store user scores
-user_scores = {}
+# Initialize a dictionary to store user quiz data
+user_quiz_data = {}
 
 # Start command handler
 
@@ -36,93 +36,76 @@ def start(update: Update, context: CallbackContext):
     # Shuffle the quiz questions
     random.shuffle(quiz_questions)
 
-    # Create a list to store user's answers
-    user_answers = []
-
-    for question_data in quiz_questions:
-        # Create inline keyboard buttons for quiz options
-        options = [
-            InlineKeyboardButton(
-                "1. " + question_data["option1"], callback_data="1"),
-            InlineKeyboardButton(
-                "2. " + question_data["option2"], callback_data="2"),
-            InlineKeyboardButton(
-                "3. " + question_data["option3"], callback_data="3"),
-            InlineKeyboardButton(
-                "4. " + question_data["option4"], callback_data="4"),
-        ]
-
-        # Create an inline keyboard markup
-        reply_markup = InlineKeyboardMarkup([options])
-
-        # Send the quiz question with options
-        message = context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=question_data["question"],
-            reply_markup=reply_markup,
-        )
-
-        user_answers.append({
-            "message_id": message.message_id,
-            "correct_option": question_data["correct_option"]
-        })
-
-    # Store the user's answers for later checking
-    user_scores[user_id] = {
-        "score": 0,
-        "user_answers": user_answers,
-        "current_question": 0
+    # Store the shuffled questions for later use
+    user_quiz_data[user_id] = {
+        "remaining_questions": quiz_questions,
     }
 
-# Callback function to handle user answers
+    # Send the first quiz question
+    send_next_question(update, context)
+
+# Function to send the next quiz question
 
 
-def answer_quiz(update: Update, context: CallbackContext):
-    query = update.callback_query
-    user_id = query.from_user.id
-    user_data = user_scores.get(user_id)
+def send_next_question(update: Update, context: CallbackContext):
+    user_id = update.effective_user.id
+    user_data = user_quiz_data.get(user_id)
 
-    if user_data is not None and user_data["current_question"] < len(user_data["user_answers"]):
-        current_question_index = user_data["current_question"]
-        user_answer = int(query.data)
-        correct_option = user_data["user_answers"][current_question_index]["correct_option"]
+    if user_data and user_data["remaining_questions"]:
+        question_data = user_data["remaining_questions"].pop(0)
+        options = [question_data["option1"], question_data["option2"],
+                   question_data["option3"], question_data["option4"]]
+        context.bot.send_poll(
+            chat_id=update.effective_chat.id,
+            question=question_data["question"],
+            options=options,
+            # Assuming correct_option is 1-indexed
+            correct_option_id=question_data["correct_option"] - 1,
+            type='quiz',
+            # Assuming 'explanation' column exists in your DB
+            explanation=question_data.get("explanation", "")
+        )
+    else:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Quiz complete!"
+        )
+        del user_quiz_data[user_id]  # Remove user data after quiz completion
 
-        if user_answer == correct_option:
-            user_data["score"] += 1
+# Poll answer handler
 
-        # Next question
-        current_question_index += 1
-        user_data["current_question"] = current_question_index
 
-        # Check if there are more questions
-        if current_question_index < len(user_data["user_answers"]):
-            question_data = user_data["user_answers"][current_question_index]
-            options = [
-                InlineKeyboardButton(
-                    "1. " + question_data["option1"], callback_data="1"),
-                InlineKeyboardButton(
-                    "2. " + question_data["option2"], callback_data="2"),
-                InlineKeyboardButton(
-                    "3. " + question_data["option3"], callback_data="3"),
-                InlineKeyboardButton(
-                    "4. " + question_data["option4"], callback_data="4"),
-            ]
+def handle_poll_answer(update: Update, context: CallbackContext):
+    user_id = update.effective_user.id
+    user_data = user_quiz_data.get(user_id)
 
-            reply_markup = InlineKeyboardMarkup([options])
-            context.bot.edit_message_text(
-                chat_id=update.effective_chat.id,
-                message_id=query.message.message_id,
-                text=question_data["question"],
-                reply_markup=reply_markup,
-            )
-        else:
-            final_score = user_data["score"]
+    if user_data and user_data["remaining_questions"]:
+        # Get the data for the next question
+        # Do not pop the question yet
+        question_data = user_data["remaining_questions"][0]
+
+        # Send the explanation for the previous question
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Explanation: {question_data['explanation']}"
+        )
+
+        # Now send the next question
+        send_next_question(update, context)
+    else:
+        # Send the explanation for the last question
+        if user_data:
+            last_question_data = user_data["remaining_questions"].pop()
             context.bot.send_message(
-                chat_id=update.effective_chat.id, text=f"Your final score: {final_score}")
-            del user_scores[user_id]
+                chat_id=update.effective_chat.id,
+                text=f"Explanation: {last_question_data['explanation']}"
+            )
 
-    # Answer the callback query to remove the inline keyboard
-    query.answer()
+        # Notify the user that the quiz is complete
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Quiz complete!"
+        )
+        del user_quiz_data[user_id]  # Remove user data after quiz completion
+
 
 # Set up the Telegram bot and handlers
 
@@ -132,7 +115,7 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CallbackQueryHandler(answer_quiz))
+    dispatcher.add_handler(PollAnswerHandler(handle_poll_answer))
 
     updater.start_polling()
     updater.idle()
